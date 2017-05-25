b0b9b0f8870b356541f442261fbfdf60a6aedc8c
GlowBlood <ipberezovskaya@yandex.ru>
2017-05-25 16:04:51 +0300

    Новые исправления

--- src/main/java/uz/sofia/Ciphering.java
+++ src/main/java/uz/sofia/Ciphering.java
@@ -1,17 +1,6 @@
-
-
 package uz.sofia;
+
 import java.io.*;
-
-/*Шифрация (-с) или дешифрация (-d) указанного файла. Выходной файл указывается как -o filename.txt,
-по умоланию имя формируется из имени входного файла сдобавлением расширения.
-Алгоритм шифрации XOR. Ключ указывается после -c или -d в шестнадцатиричной системе, длина ключа - любое целое количество байт.
-
-Вид коммандной строки:
-Ciphering [-c key] [-d key] inputname.txt [-o outputename.txt]
-
-Командная строка для шифрации:
--c cafe in.txt -o out.txt Командная строка для дешифрации: -d cafe out.txt */
 
 public class Ciphering {
     private static String outputFileName;
@@ -50,30 +39,43 @@ public class Ciphering {
             System.out.println("Incorrect input");
             System.exit(0);
         }
-        key = getBytesFromKey(args[0]);
 
-        boolean isFile = false;
-        if (args[1].equals("-c")) {
-            inputStream = new ByteArrayInputStream(args[2].getBytes());
+        if (args[0].equals("-c")) {
+            try {
+                key = args[1].getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream = new FileInputStream(args[2]);
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+                System.exit(0);
+            }
+            if (args.length == 3) {
+                outputFileName = "out.txt";
+            } else if (args.length == 5 && args[3].equals("-o")) {
+                outputFileName = args[4];
+            } else {
+                System.out.println("Incorrect input");
+                System.exit(0);
+            }
         } else {
-            isFile = true;
+            key = getBytesFromKey(args[0]);
             try {
                 inputStream = new FileInputStream(args[1]);
             } catch (FileNotFoundException e) {
                 System.out.println("File not found");
                 System.exit(0);
             }
-        }
-
-        if (!isFile && args.length == 3) {
-            outputFileName = "out.txt";
-        } else if (isFile && args.length == 2) {
-            outputFileName = args[1] + ".out";
-        } else if ((!isFile && args.length == 5 && args[3].equals("-o")) || (isFile && args.length == 4 && args[2].equals("-o"))) {
-            outputFileName = isFile ? args[3] : args[4];
-        } else {
-            System.out.println("Incorrect input");
-            System.exit(0);
+            if (args.length == 2) {
+                outputFileName = "out.txt";
+            } else if (args.length == 4 && args[2].equals("-o")) {
+                outputFileName = args[3];
+            } else {
+                System.out.println("Incorrect input");
+                System.exit(0);
+            }
         }
         outputFile = new File(outputFileName);
     }
--- src/test/java/TestCiphering.java
+++ src/test/java/TestCiphering.java
@@ -5,20 +5,149 @@ import java.io.*;
 
 public class TestCiphering {
     @Test
-    public void testSimpleString() {
-        String test = "abcdef";
-        Ciphering.main(("cafe -c " + test + " -o in.txt").split(" "));
+    public void testEmptyFile() {
+        String filename = "empty";
+        File empty = new File(filename);
+        try {
+            empty.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Ciphering.main(("cafe " + filename + " -o in.txt").split(" "));
         Ciphering.main("cafe in.txt -o out.txt".split(" "));
         try {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("out.txt"));
-            int i = 0;
-            int b;
-            while ((b = bufferedInputStream.read()) >= 0) {
-                if (test.getBytes()[i++] != b) {
+            BufferedInputStream bufferedInputStream1 = new BufferedInputStream(new FileInputStream("out.txt"));
+            BufferedInputStream bufferedInputStream2 = new BufferedInputStream(new FileInputStream(filename));
+            int b1 = 0;
+            int b2 = 0;
+            while ((b1 = bufferedInputStream1.read()) >= 0 & (b2 = bufferedInputStream2.read()) >= 0) {
+                if (b1 != b2) {
                     assert false;
                 }
             }
-            if (i != test.getBytes().length) {
+            if (b1 != b2) {
+                assert false;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new File("in.txt").delete();
+        new File("out.txt").delete();
+        empty.delete();
+        assert true;
+    }
+
+    @Test
+    public void testFileWithSmallContent() {
+        String filename = "small";
+        File small = new File(filename);
+        try {
+            small.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileOutputStream outputStream = new FileOutputStream(small);
+            outputStream.write("abc".getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Ciphering.main(("cafe123456 " + filename + " -o in.txt").split(" "));
+        Ciphering.main("cafe123456 in.txt -o out.txt".split(" "));
+        try {
+            BufferedInputStream bufferedInputStream1 = new BufferedInputStream(new FileInputStream("out.txt"));
+            BufferedInputStream bufferedInputStream2 = new BufferedInputStream(new FileInputStream(filename));
+            int b1 = 0;
+            int b2 = 0;
+            while ((b1 = bufferedInputStream1.read()) >= 0 & (b2 = bufferedInputStream2.read()) >= 0) {
+                if (b1 != b2) {
+                    assert false;
+                }
+            }
+            if (b1 != b2) {
+                assert false;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new File("in.txt").delete();
+        new File("out.txt").delete();
+        small.delete();
+        assert true;
+    }
+
+    @Test
+    public void testFileWithContentLengthEqualsToKeyLenght() {
+        String filename = "small";
+        File small = new File(filename);
+        try {
+            small.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileOutputStream outputStream = new FileOutputStream(small);
+            outputStream.write("abcd".getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Ciphering.main(("cafe " + filename + " -o in.txt").split(" "));
+        Ciphering.main("cafe in.txt -o out.txt".split(" "));
+        try {
+            BufferedInputStream bufferedInputStream1 = new BufferedInputStream(new FileInputStream("out.txt"));
+            BufferedInputStream bufferedInputStream2 = new BufferedInputStream(new FileInputStream(filename));
+            int b1 = 0;
+            int b2 = 0;
+            while ((b1 = bufferedInputStream1.read()) >= 0 & (b2 = bufferedInputStream2.read()) >= 0) {
+                if (b1 != b2) {
+                    assert false;
+                }
+            }
+            if (b1 != b2) {
+                assert false;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new File("in.txt").delete();
+        new File("out.txt").delete();
+        small.delete();
+        assert true;
+    }
+
+    @Test
+    public void testFile() {
+        String test = "src" + File.separator + "Ciphering.java";
+        Ciphering.main(("cafe " + test + " -o in.txt").split(" "));
+        Ciphering.main("cafe in.txt -o out.txt".split(" "));
+        try {
+            BufferedInputStream bufferedInputStream1 = new BufferedInputStream(new FileInputStream("out.txt"));
+            BufferedInputStream bufferedInputStream2 = new BufferedInputStream(new FileInputStream(test));
+            int b1 = 0;
+            int b2 = 0;
+            while ((b1 = bufferedInputStream1.read()) >= 0 & (b2 = bufferedInputStream2.read()) >= 0) {
+                if (b1 != b2) {
+                    assert false;
+                }
+            }
+            if (b1 != b2) {
                 assert false;
             }
 
@@ -33,10 +162,11 @@ public class TestCiphering {
     }
 
     @Test
-    public void testFile() {
+    public void testFileWithStringKey() {
+        String key = "alppimfmclpp./,,.as";
         String test = "src" + File.separator + "Ciphering.java";
-        Ciphering.main(("cafe " + test + " -o in.txt").split(" "));
-        Ciphering.main("cafe in.txt -o out.txt".split(" "));
+        Ciphering.main(("-c " + key + " " + test + " -o in.txt").split(" "));
+        Ciphering.main(("-c " + key + " in.txt -o out.txt").split(" "));
         try {
             BufferedInputStream bufferedInputStream1 = new BufferedInputStream(new FileInputStream("out.txt"));
             BufferedInputStream bufferedInputStream2 = new BufferedInputStream(new FileInputStream(test));
